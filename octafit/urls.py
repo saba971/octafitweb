f1ace755ae11c7e41026cfba3efe99e36e536f90
sabanayagam T <38027284+saba971@users.noreply.github.com>
2022-03-26 16:28:46 +0530

    Add files via upload

---
+++ octafit/urls.py
@@ -0,0 +1,33 @@
+"""octafit URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from welcome import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #toobar pattern
+    path('__debug__/', include('debug_toolbar.urls')),
+    #include will route to our app(welcome)
+    path('welcome/',include('welcome.urls')),
+    #name  is given for url redirect.(if needed)
+    path('', views.home,name="home"),
+    path('register', views.register,name="register"),
+    path('clientcount',views.clientcount),
+    path('view',views.view,name="view"),
+    path('view2',views.view2,name="view")
+]
